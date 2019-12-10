2031f7f0c660177fa65ac3f84d0a2189cc074a4a
stefani <stefanimarieangel@gmail.com>
2019-12-10 06:47:25 -0500

    revision to ArrayList

--- ArrayListPractice/src/com/revature/list/MyArrayList.java
+++ ArrayListPractice/src/com/revature/list/MyArrayList.java
@@ -1,38 +1,44 @@
 package com.revature.list;
 
 public class MyArrayList {
-  
-  private int size = 0;
-  private int[] myArray = new int[100];
-  
+
+	private int size = 0;
+	private int[] myArray = new int[100];
 
 	public void add(int val) {
-		  // Create temp array with an extra slot
-      if(size == myArray.length) {
-        int[] temp = new int[size + 1];
-       // Repopulate array with original values
-        for(int i = 0; i < size; i++) {
-          temp[i] =  myArray[i];
-        }
-        // Add new Value
-        temp[size] = val;
-        size++;
-        // change myArray to temp
-        myArray = temp;
-      } else {
-        myArray[size] = val;
-        size++;
-      }
+		// Create temp array with an extra slot
+		if (size == myArray.length) {
+			int[] temp = new int[size + 1];
+			// Repopulate array with original values
+			for (int i = 0; i < size; i++) {
+				temp[i] = myArray[i];
+			}
+			// Add new Value
+			temp[size] = val;
+			size++;
+			// change myArray to temp
+			myArray = temp;
+		} else {
+			myArray[size] = val;
+			size++;
+		}
 	}
 
 	public void set(int index, int val) {
 		// TODO Auto-generated method stub
-    myArray[index] = val;
+		myArray[index] = val;
 	}
 
 	public void remove(int index) {
-		// TODO Auto-generated method stub
-		
+		int[] tempArr = new int[myArray.length - 1];
+		for (int i = 0; i < index; i++) {
+			tempArr[i] = myArray[i];
+		}
+		for (int i = index + 1; i < myArray.length; i++) {
+			tempArr[i - 1] = myArray[i];
+		}
+		myArray = tempArr;
+
 	}
 
 	public int get(int index) {
@@ -42,7 +48,13 @@ public class MyArrayList {
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder("[" + myArray[0]);
+		for (int i = 1; i < myArray.length; i++) {
+			sb.append(", " + myArray[i]);
+		}
+		sb.append("]");
+
+		String s = new String(sb);
+		return s;
 	}
 }
